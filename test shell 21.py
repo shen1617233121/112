d2800474ff762108542650c5c5e995983d3c37e3
unknown <809386174@qq.com>
2017-11-01 09:33:58 +0800

    test too

---
+++ test shell 21.py
@@ -0,0 +1,11 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+======================== RESTART: D:/16321/test21.py ========================
+[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed (_ssl.c:661)
+
+Traceback (most recent call last):
+  File "D:/16321/test21.py", line 23, in <module>
+    content_list = re.findall(content_pattern, html)
+NameError: name 'html' is not defined
+>>> 
