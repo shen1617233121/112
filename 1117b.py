1fa0d5205afd3fbf4a8e5204ed16bf31ec553603
unknown <809386174@qq.com>
2017-11-17 09:10:02 +0800

    test

---
+++ 1117b.py
@@ -0,0 +1,8 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: C:/Users/lenovo32/Desktop/1117a.py ================
+Error:没有找到测试文件或读取文件失败
+[Errno 2] No such file or directory: 'C:/code/tmp1/testfile.txt'
+go on.......
+>>> 
